618707a70fb430f6f81f4a8ea84f8d9747c4d2bf
AstroGrey <65878190+AstroGrey@users.noreply.github.com>
2021-05-20 11:30:48 -0700

    Delete problemsDBController.ts

--- problemsDBController.ts
+++
@@ -1,9 +0,0 @@
-import {getCustomRepository} from "typeorm";
-import {problemRepository} from "../database/problemsRepository";
-import { createConnection } from "typeorm";
-
-export class problemsDBController{
-    //private problemRepository: problemRepository;
-
-
-}
