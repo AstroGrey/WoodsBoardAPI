c75d81ce182e572e5377df22743881690813afab
Zach Rodreick <65878190+AstroGrey@users.noreply.github.com>
2022-01-08 19:35:49 -0800

    Delete dto directory

--- dto/patch.problem.dto.ts
+++
@@ -1,3 +0,0 @@
-import { PutProblemDto } from './put.problems.dto';
-
-export interface PatchProblemDto extends Partial<PutProblemDto> {}
--- dto/put.user.dto.ts
+++
@@ -1,9 +0,0 @@
-export interface PutUserDto {
-    id: string;
-    email: string;
-    password: string;
-    username: string;
-    firstName: string;
-    lastName: string;
-    permissionLevel: number;
-}
